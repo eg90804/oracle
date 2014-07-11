872791497793dc769fd81c130bc735397ef41d78
hajee <hajee@moretIA.com>
2014-07-11 12:54:23 +0200

    Pass the full datafile name to sqlplus fixes #18
    Signed-off-by: hajee <hajee@moretIA.com>

--- lib/puppet/type/tablespace/datafile.rb
+++ lib/puppet/type/tablespace/datafile.rb
@@ -1,9 +1,10 @@
 newproperty(:datafile) do
+
   include EasyType
   desc "The name of the datafile"
 
   to_translate_to_resource do | raw_resource|
-    File.basename(raw_resource.column_data('FILE_NAME'))
+    raw_resource.column_data('FILE_NAME')
   end
 
 end
