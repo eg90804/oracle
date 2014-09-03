2641bd30da91e9c17f3d3325e0f44e225740af75
Bert Hajee <hajee@moretIA.com>
2014-09-03 18:15:20 +0200

    Fixed support for scripts on oracle_exec

--- lib/puppet/provider/oracle_exec/sqlplus.rb
+++ lib/puppet/provider/oracle_exec/sqlplus.rb
@@ -4,25 +4,23 @@ require 'easy_type/helpers'
 Puppet::Type.type(:oracle_exec).provide(:sqlplus) do
   include Utils::OracleAccess
   include EasyType::Helpers
-  include Puppet::Util::Logging
+  include EasyType::Template
 
   mk_resource_methods
 
   def flush
-  	if is_script?(statement)
-  		sql_statements = template(statement, binding)
-	    output = sql sql_statements, :username => resource.username, :password => resource.password, :sid => resource.sid
-  	else
-	    output = sql statement, :username => resource.username, :password => resource.password, :sid => resource.sid
-	  end
-	end
+    if is_script?(statement)
+      script_name = statement.sub('@','')
+      statement = template(script_name, binding)
+    end
+    output = sql statement, :username => resource.username, :password => resource.password, :sid => resource.sid
     Puppet.info(output) if resource.logoutput == :true
   end
 
   private
 
   def is_script?(statement)
-  	statement[0] == '@'
+    statement[0] == 64
   end
 
 end
