891044694e47b4fce13e8c0d4bcb0984878cfca9
Bert Hajee <hajee@moretIA.com>
2014-10-14 08:42:32 +0200

    Compatibility checks only equalness for given value

--- lib/puppet/type/asm_diskgroup/compat_asm.rb
+++ lib/puppet/type/asm_diskgroup/compat_asm.rb
@@ -9,4 +9,15 @@ newproperty(:compat_asm) do
     raw_resource.column_data('COMPATIBILITY').upcase
   end
 
+  #
+  # If the string length of the actual value is longer the the string length of the specified value,
+  # only check if they are equal for the given string
+  # example:
+  #  12.1 == 12.1.0.0 
+  #
+  def insync?(is)
+  	length = should.length
+  	is.slice(0,length) == should
+  end
+
 end
--- lib/puppet/type/asm_diskgroup/compat_rdbms.rb
+++ lib/puppet/type/asm_diskgroup/compat_rdbms.rb
@@ -9,4 +9,16 @@ newproperty(:compat_rdbms) do
     raw_resource.column_data('DATABASE_COMPATIBILITY').upcase
   end
 
+  #
+  # If the string length of the actual value is longer the the string length of the specified value,
+  # only check if they are equal for the given string
+  # example:
+  #  12.1 == 12.1.0.0 
+  #
+  def insync?(is)
+  	length = should.length
+  	is.slice(0,length) == should
+  end
+
+
 end
