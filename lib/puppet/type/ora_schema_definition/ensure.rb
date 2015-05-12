844220cb4a28e5c4dc4d9035a669f40eab9df435
Bert Hajee <hajee@moretIA.com>
2015-05-12 16:36:05 +0200

    Initial implementation of ora_schema_definition

---
+++ lib/puppet/type/ora_schema_definition/ensure.rb
@@ -0,0 +1,18 @@
+newproperty(:ensure) do
+  include EasyType
+
+  desc "version number or latest"
+
+  newvalue(:absent) do
+    @resource.provider.destroy
+  end
+  newvalue(:latest)
+  newvalue(/^\d{1,3}.\d{1,3}.\d{1,3}$/) 
+
+  defaultto(:latest)
+
+  to_translate_to_resource do | raw_resource|
+    raw_resource.column_data('VERSION')
+  end
+
+end
